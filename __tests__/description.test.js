743fae0b545f5232ae873cce7815a8e0b6699c10
Shine Lee <aungshine@gmail.com>
2018-05-21 08:37:50 -0700

    (feat) description advanced settings implemented (#44)

--- __tests__/description.test.js
+++ __tests__/description.test.js
@@ -24,10 +24,42 @@ test('description is correct', async () => {
   let descriptionValidation = await description(createMockPR(''), null, config.settings)
 
   expect(descriptionValidation.mergeable).toBe(false)
-  expect(descriptionValidation.description).toBe(`Description on the pull request should not be empty`)
+  expect(descriptionValidation.description[0]).toBe("The Description can't be empty")
 
   descriptionValidation = await description(createMockPR('Non empty Description'), null, config.settings)
   expect(descriptionValidation.description).toBe(null)
+})
+
+test('must_include works', async () => {
+  let config = new Configuration(`
+    mergeable:
+      description:
+        must_include:
+          regex: 'test string'
+          message: 'failed test'
+  `)
+  let descriptionValidation = await description(createMockPR('test string included'), null, config.settings)
+  expect(descriptionValidation.mergeable).toBe(true)
+
+  descriptionValidation = await description(createMockPR('Non empty Description'), null, config.settings)
+  expect(descriptionValidation.mergeable).toBe(false)
+  expect(descriptionValidation.description[0]).toBe('failed test')
+})
+
+test('must_exclude works', async () => {
+  let config = new Configuration(`
+    mergeable:
+      description:
+        must_exclude:
+          regex: 'test string'
+          message: 'failed test'
+  `)
+  let descriptionValidation = await description(createMockPR('test string included'), null, config.settings)
+  expect(descriptionValidation.mergeable).toBe(false)
+  expect(descriptionValidation.description[0]).toBe('failed test')
+
+  descriptionValidation = await description(createMockPR('Non empty Description'), null, config.settings)
+  expect(descriptionValidation.mergeable).toBe(true)
 })
 
 const createMockPR = (description) => {
