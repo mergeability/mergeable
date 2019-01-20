349ee827dee5b853b52287fdcfbd07049ec8cca3
jusx <hello@justinlaw.org>
2019-01-20 14:21:10 -0800

    fix(stale): ensure that stale sends in the correct params.

--- __tests__/validators/stale.test.js
+++ __tests__/validators/stale.test.js
@@ -1,11 +1,14 @@
 const Helper = require('../../__fixtures__/helper')
 const Stale = require('../../lib/validators/stale')
 
+// Stale relies on the search api:
+// https://help.github.com/articles/searching-issues-and-pull-requests/#search-only-issues-or-pull-requests
+// Tests should ensure that the parameters passed into the API is accurate
 test('will set the issues and pulls appropriately when both types are specified', async () => {
   let settings = {
     do: 'stale',
     days: 10,
-    type: ['issues', 'pull_request']
+    type: ['issues', 'pull_request'] // this is valid when using the new config.
   }
 
   let stale = new Stale()
@@ -15,7 +18,7 @@ test('will set the issues and pulls appropriately when both types are specified'
   ])
 
   let results = await stale.validate(context, settings)
-
+  expect(isParamsNoType(context)).toBe(true) // it includes both types.
   expect(results.schedule.issues.length).toBe(1)
   expect(results.schedule.pulls.length).toBe(1)
 })
@@ -30,6 +33,8 @@ test('will set the issues and pulls appropriately when no type is set', async ()
   ])
 
   let results = await stale.validate(context, settings)
+  // no types specified so we assume both types. Hence none passed to search API.
+  expect(isParamsNoType(context)).toBe(true)
   expect(results.schedule.issues.length).toBe(1)
   expect(results.schedule.pulls.length).toBe(1)
 })
@@ -44,14 +49,13 @@ test('will set the issues and pulls even when unsupported type is set', async ()
   ])
 
   let results = await stale.validate(context, settings)
-  let callParam = context.github.search.issues.mock.calls
-  expect(callParam[0][0].q.includes('type')).toBeFalsy()
+  expect(isParamsNoType(context)).toBe(true) // if non supported is specified we still don't specify.
   expect(results.schedule.issues).toBeDefined()
   expect(results.schedule.pulls).toBeDefined()
 
   settings.type = ['junk', 'issues']
   results = await stale.validate(context, settings)
-  expect(callParam[1][0].q.includes('type:issues')).toBeTruthy()
+  expect(getFilteredParams(context, 'type:issue').length).toBe(1) // should have called with type:issue
   expect(results.schedule.issues).toBeDefined()
   expect(results.schedule.pulls).toBeDefined()
 })
@@ -67,7 +71,8 @@ test('will set the issues and pulls correctly when type is issue only', async ()
   let context = createMockContext([{ number: 1 }])
 
   let res = await stale.validate(context, settings)
-
+  expect(getFilteredParams(context, 'type:issue').length).toBe(1)
+  expect(getFilteredParams(context, 'type:pr').length).toBe(0)
   expect(res.schedule.issues.length).toBe(1)
   expect(res.schedule.pulls.length).toBe(0)
 })
@@ -83,9 +88,25 @@ test('will set the issues and pulls correctly when type is pull_request only', a
   let context = createMockContext([{ number: 1, pull_request: {} }])
 
   let res = await stale.validate(context, settings)
+  expect(getFilteredParams(context, 'type:issue').length).toBe(0)
+  expect(getFilteredParams(context, 'type:pr').length).toBe(1)
   expect(res.schedule.pulls.length).toBe(1)
   expect(res.schedule.issues.length).toBe(0)
 })
+
+const getFilteredParams = (context, filter = '', days = 10) => {
+  let callParams = context.github.search.issues.mock.calls
+  let timestamp = (new Date(new Date() - days * 24 * 60 * 60 * 1000)).toISOString().replace(/\.\d{3}\w$/, '')
+  let q = `repo:owner/repo is:open updated:<${timestamp} ${filter}`.trim()
+  return callParams.filter(param => param[0].q === q)
+}
+
+const isParamsNoType = (context) => {
+  return context.github.search.issues.mock.calls
+    .filter(
+      param => param[0].q.includes('type:')
+    ).length === 0
+}
 
 const createMockContext = (results) => {
   let context = Helper.mockContext()
