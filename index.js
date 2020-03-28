b9aa739967aa05713ed3e76276459111c7f0a51c
shine <aungshine@gmail.com>
2020-03-27 20:58:02 -0700

    set up logging module and structure

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Mergeable } = require('./lib/mergeable')
+const logger = require('./lib/logger')
 
 module.exports = (robot) => {
+  logger.init(robot.log)
   let mergeable = new Mergeable(process.env.NODE_ENV)
   mergeable.start(robot)
 }
