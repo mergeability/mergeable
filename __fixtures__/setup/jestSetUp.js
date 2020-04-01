8f8fdb8cbb58fb9d70cbab9129fe5f99da57e178
shine <aungshine@gmail.com>
2020-03-31 22:34:18 -0700

    address jusx's comments

--- __fixtures__/setup/jestSetUp.js
+++ __fixtures__/setup/jestSetUp.js
@@ -1,4 +1,6 @@
 // mock Logger
+const mockLogTypes = require('../../lib/utils/logTypes')
+
 jest.mock('../../lib/logger.js', () => {
   return {
     init: jest.fn(),
@@ -11,15 +13,6 @@ jest.mock('../../lib/logger.js', () => {
         fatal: jest.fn()
       }
     },
-    logTypes: {
-      EVENT_RECEIVED: 'event_received',
-      CONFIG_INVALID_YML: 'config_invalid_yml',
-      UNKNOWN_ERROR_VALIDATOR: 'unknown_error_validator',
-      UNKNOWN_ERROR_ACTION: 'unknown_error_action',
-      VALIDATOR_PROCESS: 'validator_process',
-      ACTION_BEFORE_VALIDATE_EXECUTE: 'action_before_validate_execute',
-      ACTION_AFTER_VALIDATE_EXECUTE: 'action_after_validate_execute',
-      CONFIG: 'config'
-    }
+    logTypes: mockLogTypes
   }
 })
