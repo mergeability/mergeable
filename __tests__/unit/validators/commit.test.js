4f539bd266d317af7d184085cc292ea3b635b3f2
shine <aungshine@gmail.com>
2020-04-08 13:58:37 -0700

    feat: commit validator

--- __tests__/unit/validators/commit.test.js
+++ __tests__/unit/validators/commit.test.js
@@ -2,62 +2,156 @@ const Helper = require('../../../__fixtures__/unit/helper')
 const Commit = require('../../../lib/validators/commit')
 
 test('validate returns correctly', async () => {
-  const changeset = new Commit()
+  const commit = new Commit()
   const settings = {
-    do: 'changeset',
-    must_exclude: {
-      regex: 'b.js'
+    do: 'commit',
+    message: {
+      regex: 'feat:'
     }
-
   }
+  const date = Date.now()
+  const commits = [
+    {
+      commit: {
+        author: {
+          date
+        },
+        message: 'fix: this'
+      }
+    },
+    {
+      commit: {
+        author: {
+          date: date + 1
+        },
+        message: 'feat: that'
+      }
+    }
+  ]
 
-  let validation = await changeset.validate(createMockContext(['package-lock.json', 'yarn.lock', 'package.json', 'a.js']), settings)
-  expect(validation.status).toBe('pass')
-
-  validation = await changeset.validate(createMockContext(['package-lock.json', 'yarn.lock', 'package.json', 'b.js']), settings)
+  let validation = await commit.validate(createMockContext(commits), settings)
   expect(validation.status).toBe('fail')
+
+  commits[0].commit.message = 'feat: this'
+
+  validation = await commit.validate(createMockContext(commits), settings)
+  expect(validation.status).toBe('pass')
 })
 
 test('oldest_only sub option', async () => {
-  const changeset = new Commit()
+  const commit = new Commit()
   const settings = {
-    do: 'changeset',
-    must_include: {
-      regex: '@#$@#$@#$'
+    do: 'commit',
+    message: {
+      regex: 'feat:',
+      oldest_only: true
     }
-
   }
+  const date = Date.now()
+  const commits = [
+    {
+      commit: {
+        author: {
+          date
+        },
+        message: 'fix: this'
+      }
+    },
+    {
+      commit: {
+        author: {
+          date: date + 1
+        },
+        message: 'fix: that'
+      }
+    }
+  ]
 
-  let validation = await changeset.validate(createMockContext(['package-lock.json', 'yarn.lock', 'package.json', 'a.js']), settings)
+  let validation = await commit.validate(createMockContext(commits), settings)
   expect(validation.status).toBe('fail')
-})
 
-test('skip_merge sub option', async () => {
-  const changeset = new Commit()
-  const settings = {
-    do: 'changeset',
-    must_include: {
-      regex: 'a.js'
-    }
-  }
+  commits[0].commit.message = 'feat: this'
 
-  let validation = await changeset.validate(createMockContext(['package-lock.json', 'yarn.lock', 'package.json', 'a.js', 'a.jsx']), settings)
+  validation = await commit.validate(createMockContext(commits), settings)
   expect(validation.status).toBe('pass')
 })
 
-test('error handling', async () => {
-  const changeset = new Commit()
+test('skip_merge sub option', async () => {
+  const commit = new Commit()
   const settings = {
-    do: 'changeset',
-    must_exclude: {
-      regex: 'yarn.lock'
+    do: 'commit',
+    message: {
+      regex: 'feat:',
+      skip_merge: false
     }
   }
+  const date = Date.now()
+  const commits = [
+    {
+      commit: {
+        author: {
+          date
+        },
+        message: 'feat: this'
+      }
+    },
+    {
+      commit: {
+        author: {
+          date: date + 1
+        },
+        message: 'Merge branch'
+      }
+    }
+  ]
 
-  let validation = await changeset.validate(createMockContext(['package-lock.json', 'yarn.lock', 'package.json', 'a.js']), settings)
+  let validation = await commit.validate(createMockContext(commits), settings)
   expect(validation.status).toBe('fail')
+
+  settings.message.skip_merge = true
+  validation = await commit.validate(createMockContext(commits), settings)
+  expect(validation.status).toBe('pass')
 })
 
-const createMockContext = (files) => {
-  return Helper.mockContext({files: files})
+test('single_commit_only sub option', async () => {
+  const commit = new Commit()
+  const settings = {
+    do: 'commit',
+    message: {
+      regex: 'feat:',
+      single_commit_only: true
+    }
+  }
+  const date = Date.now()
+  const commits = [
+    {
+      commit: {
+        author: {
+          date
+        },
+        message: 'fix: this'
+      }
+    },
+    {
+      commit: {
+        author: {
+          date: date + 1
+        },
+        message: 'test'
+      }
+    }
+  ]
+
+  let validation = await commit.validate(createMockContext(commits), settings)
+  expect(validation.status).toBe('pass')
+  expect(validation.validations[0].description).toBe('Since there are more than one commits, Skipping validation')
+
+  commits.pop()
+  validation = await commit.validate(createMockContext(commits), settings)
+  expect(validation.status).toBe('fail')
+  expect(validation.validations[0].description).toBe('Some or all of your commit messages doesn\'t meet the criteria')
+})
+
+const createMockContext = (commits) => {
+  return Helper.mockContext({commits})
 }
