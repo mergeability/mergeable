69ce004b87f63abdc9bc40592a8716d608fe86c4
Sambhav Kothari <sambhavs.email@gmail.com>
2020-10-08 01:15:02 +0100

    fix: Do not attempt to merge a pull request if the status is blocked (#400)

--- __tests__/unit/actions/merge.test.js
+++ __tests__/unit/actions/merge.test.js
@@ -5,6 +5,7 @@ test('check that merge is called if PR has not been merged', async () => {
   const merge = new Merge()
   const checkIfMerged = false
   const context = Helper.mockContext({ checkIfMerged })
+  context.github.pulls.get.mockReturnValue({ data: { mergeable_state: 'clean', state: 'open' } })
   const settings = {}
 
   await merge.afterValidate(context, settings)
@@ -12,10 +13,33 @@ test('check that merge is called if PR has not been merged', async () => {
   expect(context.github.pulls.merge.mock.calls[0][0].merge_method).toBe('merge')
 })
 
+test('check that merge is not called if PR is in a blocked state', async () => {
+  const merge = new Merge()
+  const checkIfMerged = false
+  const context = Helper.mockContext({ checkIfMerged })
+  context.github.pulls.get.mockReturnValue({ data: { mergeable_state: 'blocked', state: 'open' } })
+  const settings = {}
+
+  await merge.afterValidate(context, settings)
+  expect(context.github.pulls.merge.mock.calls.length).toBe(0)
+})
+
+test('check that merge is not called if PR is closed', async () => {
+  const merge = new Merge()
+  const checkIfMerged = false
+  const context = Helper.mockContext({ checkIfMerged })
+  context.github.pulls.get.mockReturnValue({ data: { mergeable_state: 'clean', state: 'closed' } })
+  const settings = {}
+
+  await merge.afterValidate(context, settings)
+  expect(context.github.pulls.merge.mock.calls.length).toBe(0)
+})
+
 test('check that merge is not called if PR has not been merged', async () => {
   const merge = new Merge()
   const checkIfMerged = true
   const context = Helper.mockContext({ checkIfMerged })
+  context.github.pulls.get.mockReturnValue({ data: { mergeable_state: 'clean', state: 'open' } })
   const settings = {}
 
   await merge.afterValidate(context, settings)
@@ -26,6 +50,7 @@ test('check that merge_method option works correctly', async () => {
   const merge = new Merge()
   const checkIfMerged = false
   const context = Helper.mockContext({ checkIfMerged })
+  context.github.pulls.get.mockReturnValue({ data: { mergeable_state: 'clean', state: 'open' } })
   const settings = {
     merge_method: 'squash'
   }
