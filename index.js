bb5034a770400e9c18f6ec3a7daa9be550182e4e
Sean Smith <sean@wwsean08.com>
2021-09-20 10:06:44 -0700

    fix: Check that getRouter is defined before attempting to access it (#587)

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const githubRateLimitEndpoint = require('./lib/utils/githubRateLimitEndpoint')
 const prometheusMiddleware = require('express-prometheus-middleware')
 
 module.exports = (robot, { getRouter }) => {
-  const router = getRouter()
+  if (getRouter !== undefined) {
+    const router = getRouter()
 
-  if (process.env.ENABLE_GITHUB_RATELIMIT_ENDPOINT === 'true') {
-    // endpoint to fetch github given installation rate limit
-    router.get('/github-ratelimit/:installationId', githubRateLimitEndpoint(robot))
-  }
+    if (process.env.ENABLE_GITHUB_RATELIMIT_ENDPOINT === 'true') {
+      // endpoint to fetch github given installation rate limit
+      router.get('/github-ratelimit/:installationId', githubRateLimitEndpoint(robot))
+    }
 
-  if (process.env.ENABLE_METRICS_ENDPOINT === 'true') {
-    // expose prometheus metrics
-    router.use(prometheusMiddleware())
+    if (process.env.ENABLE_METRICS_ENDPOINT === 'true') {
+      // expose prometheus metrics
+      router.use(prometheusMiddleware())
+    }
   }
 
   logger.init(robot.log)
