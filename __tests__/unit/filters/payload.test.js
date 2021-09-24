9c0e531590c8031fddb66796ffaa77c8c3f3a1ee
Jared Hobbs <j.hobbs@carta.com>
2021-09-23 18:47:06 -0600

    feat: add support for objects in arrays in the payload filter

--- __tests__/unit/filters/payload.test.js
+++ __tests__/unit/filters/payload.test.js
@@ -49,7 +49,7 @@ test('that checks are done against correct field', async () => {
   expect(filter.status).toBe('fail')
 })
 
-test('that proper errors are returned if the field does not exit', async () => {
+test('that proper errors are returned if the field does not exist', async () => {
   const payload = new Payload()
 
   const settings = {
@@ -76,4 +76,108 @@ test('that proper errors are returned if the field does not exit', async () => {
 
   const filter = await payload.processFilter(context, settings)
   expect(filter.status).toBe('error')
+})
+
+test('that checks work against objects in an array with must_include', async () => {
+  const payload = new Payload()
+
+  const settings = {
+    do: 'payload',
+    pull_request: {
+      labels: {
+        must_include: {
+          regex: 'foo',
+          key: 'name'
+        }
+      }
+    }
+  }
+
+  let context = {
+    payload: {
+      repository: {
+        full_name: 'test-repo'
+      },
+      pull_request: {
+        labels: [
+          {
+            name: 'foo'
+          }
+        ]
+      }
+    }
+  }
+
+  let filter = await payload.processFilter(context, settings)
+  expect(filter.status).toBe('pass')
+
+  context = {
+    payload: {
+      repository: {
+        full_name: 'test-repo'
+      },
+      pull_request: {
+        labels: [
+          {
+            name: 'bar'
+          }
+        ]
+      }
+    }
+  }
+
+  filter = await payload.processFilter(context, settings)
+  expect(filter.status).toBe('fail')
+})
+
+test('that checks work against objects in an array with must_exclude', async () => {
+  const payload = new Payload()
+
+  const settings = {
+    do: 'payload',
+    pull_request: {
+      labels: {
+        must_exclude: {
+          regex: 'foo',
+          key: 'name'
+        }
+      }
+    }
+  }
+
+  let context = {
+    payload: {
+      repository: {
+        full_name: 'test-repo'
+      },
+      pull_request: {
+        labels: [
+          {
+            name: 'bar'
+          }
+        ]
+      }
+    }
+  }
+
+  let filter = await payload.processFilter(context, settings)
+  expect(filter.status).toBe('pass')
+
+  context = {
+    payload: {
+      repository: {
+        full_name: 'test-repo'
+      },
+      pull_request: {
+        labels: [
+          {
+            name: 'foo'
+          }
+        ]
+      }
+    }
+  }
+
+  filter = await payload.processFilter(context, settings)
+  expect(filter.status).toBe('fail')
 })
