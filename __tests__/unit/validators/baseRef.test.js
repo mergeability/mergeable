45a15e61ba09b97b30501f3d24a505ec2015e965
Abid H. Mujtaba <amujtaba4@bloomberg.net>
2021-12-08 21:03:30 -0500

    Add support for status events to baseRef validator
    - For status events the commit sha in the payload is used to make an
      octokit request to fetch the pull requests to which the commit
      belongs.
      The pull requests are filtered to just the open ones.
      The validator than check the base refs of these open pull requests.
    - Add unit tests to cover new functionality

--- __tests__/unit/validators/baseRef.test.js
+++ __tests__/unit/validators/baseRef.test.js
@@ -1,6 +1,63 @@
 const BaseRef = require('../../../lib/validators/baseRef')
 const Helper = require('../../../__fixtures__/unit/helper')
 
+test('validateCheckSuite called for check_suite events', async () => {
+  // GIVEN
+  const baseRef = new BaseRef()
+
+  const settings = {
+    do: 'baseRef'
+  }
+
+  const returnValue = {}
+  baseRef.validateCheckSuite = jest.fn()
+  baseRef.validateCheckSuite.mockReturnValueOnce(returnValue)
+
+  // WHEN
+  const output = await baseRef.processValidate(mockCheckSuiteContext(['foo']), settings)
+
+  // THEN
+  expect(output).toBe(returnValue)
+})
+
+test('validateStatus called for status events', async () => {
+  // GIVEN
+  const baseRef = new BaseRef()
+
+  const settings = {
+    do: 'baseRef'
+  }
+
+  const returnValue = {}
+  baseRef.validateStatus = jest.fn()
+  baseRef.validateStatus.mockReturnValueOnce(returnValue)
+
+  // WHEN
+  const output = await baseRef.processValidate(mockStatusContext(['foo']), settings)
+
+  // THEN
+  expect(output).toBe(returnValue)
+})
+
+test('processOptions called for non-check-suite non-status events', async () => {
+  // GIVEN
+  const baseRef = new BaseRef()
+
+  const settings = {
+    do: 'baseRef'
+  }
+
+  const returnValue = {}
+  baseRef.processOptions = jest.fn()
+  baseRef.processOptions.mockReturnValueOnce(returnValue)
+
+  // WHEN
+  const output = await baseRef.processValidate(mockContext('foo'), settings)
+
+  // THEN
+  expect(output).toBe(returnValue)
+})
+
 test('fail gracefully if invalid regex', async () => {
   const baseRef = new BaseRef()
 
@@ -158,8 +215,129 @@ const mockContext = baseRef => {
   return context
 }
 
+test('fail when exclude regex is in baseRef of single pull request related to status', async () => {
+  const baseRef = new BaseRef()
+
+  const settings = {
+    do: 'baseRef',
+    must_exclude: {
+      regex: 'wip'
+    }
+  }
+
+  const context = mockStatusContext(['WIP foo'])
+
+  const baseRefValidation = await baseRef.processValidate(context, settings)
+  expect(baseRefValidation.status).toBe('fail')
+})
+
+test('fail when exclude regex is in one baseRef of multiple pull requests related to status', async () => {
+  const baseRef = new BaseRef()
+
+  const settings = {
+    do: 'baseRef',
+    must_exclude: {
+      regex: 'wip'
+    }
+  }
+
+  const context = mockStatusContext(['foo', 'WIP bar', 'baz'])
+
+  const baseRefValidation = await baseRef.processValidate(context, settings)
+  expect(baseRefValidation.status).toBe('fail')
+})
+
+test('pass when exclude regex is not in any baseRef of multiple pull requests related to status', async () => {
+  const baseRef = new BaseRef()
+
+  const settings = {
+    do: 'baseRef',
+    must_exclude: {
+      regex: 'wip'
+    }
+  }
+
+  const context = mockStatusContext(['foo', 'bar', 'baz'])
+
+  const baseRefValidation = await baseRef.processValidate(context, settings)
+  expect(baseRefValidation.status).toBe('pass')
+})
+
+test('fail when include regex exists and there are no pull requests related to status', async () => {
+  const baseRef = new BaseRef()
+
+  const settings = {
+    do: 'baseRef',
+    must_include: {
+      regex: 'foo'
+    }
+  }
+
+  const context = mockStatusContext([])
+
+  const baseRefValidation = await baseRef.processValidate(context, settings)
+  expect(baseRefValidation.status).toBe('fail')
+})
+
+test('pass when exclude regex is only in baseRef of a closed pull request related to status', async () => {
+  const baseRef = new BaseRef()
+
+  const settings = {
+    do: 'baseRef',
+    must_exclude: {
+      regex: 'wip'
+    }
+  }
+
+  const context = Helper.mockContext({ eventName: 'status' })
+  const pulls = {
+    data: [
+      { state: 'open', base: { ref: 'foo' } },
+      { state: 'closed', base: { ref: 'wip bar' } }
+    ]
+  }
+  context.octokit.request = jest.fn()
+  context.octokit.request.mockReturnValueOnce(pulls)
+
+  const baseRefValidation = await baseRef.processValidate(context, settings)
+  expect(baseRefValidation.status).toBe('pass')
+})
+
+test('fail with mediaType when exclude regex is in baseRef of single pull request related to status', async () => {
+  const baseRef = new BaseRef()
+
+  const settings = {
+    do: 'baseRef',
+    must_exclude: {
+      regex: 'wip'
+    },
+    mediaType: {
+      previews: ['groot']
+    }
+  }
+
+  const context = mockStatusContext(['WIP foo'])
+
+  const baseRefValidation = await baseRef.processValidate(context, settings)
+  expect(baseRefValidation.status).toBe('fail')
+
+  expect(context.octokit.request.mock.calls[0][1].mediaType.previews[0]).toBe('groot')
+})
+
 const mockCheckSuiteContext = baseRefs => {
   const context = Helper.mockContext({ eventName: 'check_suite' })
   context.payload.check_suite.pull_requests = baseRefs.map(baseRef => ({ base: { ref: baseRef } }))
+  return context
+}
+
+const mockStatusContext = baseRefs => {
+  const context = Helper.mockContext({ eventName: 'status' })
+  const pulls = {
+    data: baseRefs.map(baseRef => ({ state: 'open', base: { ref: baseRef } }))
+  }
+
+  context.octokit.request = jest.fn()
+  context.octokit.request.mockReturnValueOnce(pulls)
+
   return context
 }
